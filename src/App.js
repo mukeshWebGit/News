bb758bda5e304c1ed4febf9e05de2824d66cbe2b
mukeshWebGit <mmuskan.940@gmail.com>
2024-01-02 18:17:42 +0530

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css';   
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+  <BrowserRouter> 
+  <Routes>
+    <Route path="/" element={<Home q="All" />} /> 
+    <Route path="/Politics" element={<Home q="Politics" />} /> 
+    <Route path="/Science" element={<Home q="Science" />} /> 
+    <Route path="/Techonolgy" element={<Home q="Techonolgy" />} /> 
+    <Route path="/Sports" element={<Home q="Sports" />} /> 
+    <Route path="/Jokes" element={<Home q="Jokes" />} /> 
+    <Route path="/Cricket" element={<Home q="Cricket" />} /> 
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/child-cmpo.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class Childpage extends Component {
+     constructor(){
+        super()
+        this.state = {
+            name:'',
+            email:'',
+        }
+     }
+
+     onChnage(e){
+        this.setState({ [e.target.name] : e.target.value}); 
+     }
+     
+  render() {
+    return (
+      <div> 
+        <p>{this.state.name}</p>
+        <p>{this.state.email}</p>
+        <h1>Onchnage Input</h1>
+        <input type='text' name="name" onChange={(e)=> this.onChnage(e)}  placeholder='Enter Your Name'/>
+        <input type='text' name="email" onChange={(e)=> this.onChnage(e)}  placeholder='Enter Your Email'/>
+        <button type='buttom'>Submite</button> 
+      </div>
+    )
+  }
+}
